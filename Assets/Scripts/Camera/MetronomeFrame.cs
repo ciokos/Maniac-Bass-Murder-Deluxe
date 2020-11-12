26abed6c10ab0dccedb3d74ec1bae8e0d646e562
ender5224 <ender5224@gmail.com>
2020-11-12 17:10:38 +0100

    Updated Moving Enemy

---
+++ Assets/Scripts/Camera/MetronomeFrame.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MetronomeFrame : MonoBehaviour
+{
+
+    private Conductor conductor;
+    private GameObject[] images;
+    private Color color1; 
+    private Color color2;
+    private bool color = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        conductor = (Conductor)GameObject.FindObjectOfType<Conductor>();
+        conductor.Beat.AddListener(onBeat);
+        images = GameObject.FindGameObjectsWithTag("Frame");
+        color1 = new Color(0.280226f, 0.1004449f, 0.9339623f, 1f);
+        color2 = new Color(0.7830189f, 0.1433072f, 0.2682463f, 1f);
+    }
+
+    private void onBeat(float beatValue)
+    {
+        foreach(GameObject i in images){
+            if(color)
+                i.GetComponent<Image>().color = color2;
+            else
+                i.GetComponent<Image>().color = color1;
+        }
+        color = !color;
+    }
+
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(0.5f, 0.5f);
+        movement *= moveSpeed;
+        movement *= Time.fixedDeltaTime;
+        this.transform.position += new Vector3(movement.x, movement.y, 0);
+    }
+}
