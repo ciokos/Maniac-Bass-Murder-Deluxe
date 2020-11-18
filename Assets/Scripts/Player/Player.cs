99b79b8af3ea77a8c02940cd14e089fd4c70fe0a
Wojciech Ciok <wciokos@gmail.com>
2020-11-18 19:44:05 +0100

    added health hearts

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,32 +9,52 @@ public class Player : MonoBehaviour
 
     public float maxHealth = 100f;
     public float currentHealth;
+    private int maxHearts = 5;
+    private int currentHearts = 5;
     public Text healthText;
     private Conductor conductor;
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
 
     // Start is called before the first frame update
     void Start()
     {
         conductor = FindObjectOfType<Conductor>();
         currentHealth = maxHealth;
-        RefreshHealthText();
+        SetHearts();
+    }
+
+    private void Update()
+    {
+        for(int i = 0; i < hearts.Length; i++)
+        {
+            if(i < currentHearts)
+            {
+                hearts[hearts.Length-i-1].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[hearts.Length - i - 1].sprite = emptyHeart;
+            }
+        }
     }
 
     public void TakeDamage(float dmg)
     {
         currentHealth -= dmg;
-        RefreshHealthText();
+        SetHearts();
         if (currentHealth <= 0)
             GameOver();
-    }
-
-    private void RefreshHealthText()
-    {
-        healthText.text = ((int)currentHealth).ToString() + "/" + ((int)maxHealth).ToString();
     }
 
     private void GameOver()
     {
         SceneManager.LoadScene(2);
+    }
+
+    private void SetHearts()
+    {
+        currentHearts = (int)Mathf.Ceil(currentHealth * maxHearts / maxHealth);
     }
 }
